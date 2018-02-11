3268d039dbde39033baaf4302b201532f6f215e9
fulviocanducci <fulviocanducci@hotmail.com>
2018-02-11 21:45:51 -0200

    All AddScope

--- WebAppRazorPageDropdownList/Startup.cs
+++ WebAppRazorPageDropdownList/Startup.cs
@@ -21,6 +21,7 @@ namespace WebAppRazorPageDropdownList
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<Web.Data.DatabaseContext>();
             services.AddMvc();
         }
 
